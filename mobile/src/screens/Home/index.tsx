16248ddfab08cc5c915c35d09c9ce15bf356ab32
renanvallerio <renanvallerio@gmail.com>
2022-09-14 18:05:50 -1000

    NLW eSports - Stage 2/6 | Aula prática - High speed

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,12 +1,15 @@
-import { View, Image } from 'react-native'
+import { View, Image, FlatList } from 'react-native'
 
 import logoImg from '../../assets/logo-nlw-esports.png'
+import { GameCard } from '../../components/GameCard';
 
 import { Heading } from "../../components/Heading";
 
+import { GAMES } from "../../utils/games";
 import { styles } from './styles';
 
 export function Home() {
+
   return (
     <View style={styles.container}>
       <Image 
@@ -19,6 +22,18 @@ export function Home() {
       subtitle="Selecione o game que deseja jogar..."
       />
 
+      <FlatList 
+      data={GAMES}
+      keyExtractor={item => item.id}
+      renderItem={({item}) => (
+        <GameCard
+          data={item}
+        />        
+      )}
+      showsHorizontalScrollIndicator={false}
+      horizontal
+      contentContainerStyle={styles.contentList}
+      />
     </View>
   )
 }
